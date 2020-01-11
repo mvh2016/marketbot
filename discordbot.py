c3bbea1d466a7eb5e70cf5d633854c04f4696140
mvh2016 <58753975+mvh2016@users.noreply.github.com>
2020-01-10 19:13:58 -0500

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,15 +8,15 @@ lastHost = ''
 lastStarting = ''
 lastMessageID = ''
 lastTitle = ''
-discordToken = environ.get('discordToken')
+discordToken = environ.get.discordToken
 
 @client.event
 async def on_ready():
     print ('Bot is ready.')
     await client.change_presence(activity=game)
     embed = discord.Embed(
-        title = '**Bot is Online**',
-        colour = discord.Colour.green()
+       title = '**Bot is Online**',
+       colour = discord.Colour.green()
     )
         
     channel = client.get_channel(664965790037966897)
@@ -121,9 +121,78 @@ async def recruitmentend(ctx, arg):
         channel = client.get_channel(665180552005025802)
         await channel.send(embed=embed)
         await ctx.send(':white_check_mark: Event successfully posted, '+ctx.message.author.mention)
-        
+
+@client.command()
+async def log(ctx, arg):
+    
+    role = discord.utils.get(ctx.guild.roles, name="Event Host")
+    attendees = ''
+    
+    if role not in ctx.author.roles:
+        await ctx.send(':warning: Insufficient permissions - role required: `Event Host` '+ctx.message.author.mention)
+    else:
+        await ctx.send('> Who attended the event?')
+
+@client.event
+async def on_message(message):
+    channel = client.get_channel(665331323996733441)
+    postChannel = client.get_channel(665346430298488845)
+    msgChannel = str(message.channel)
+    goodchannel = 'event-logs'
+    if msgChannel not in goodchannel:
+        pass
+    else:
+        botAuthor = 'Market Secretary#6439'
+        msgAuthor = str(message.author)
+        if msgAuthor not in botAuthor:
+            listofstuff = message.content.split('\n')
+            if 'Username:' in listofstuff[0]:
+                username = listofstuff[0]
+                newUsername = username.split(': ')
+                finalUsername = str(newUsername[1])
+
+                if 'Attendees:' in listofstuff[1]:
+                    attendees = listofstuff[1]
+                    newattendees = attendees.split(': ')
+                    finalattendees = str(newattendees[1])
+                
+                    if 'Type of Event:' in listofstuff[2]:
+                        typeofevent = listofstuff[2]
+                        newToE = typeofevent.split(': ')
+                        finalToE = str(newToE[1])
+                    
+                        if 'Proof:' in listofstuff[3]:
+                            proof = listofstuff[3]
+                            newProof = proof.split(': ')
+                            finalProof = str(newProof[1])
+                            
+                            embed = discord.Embed(
+                                title = '**:pencil: New Event Log :pencil:**',
+                                colour = discord.Colour.blue()
+                            )
+                            embed.add_field(name = 'Username', value = '{}'.format(finalUsername), inline = 'false')
+                            embed.add_field(name = 'Attendees', value = '{}'.format(finalattendees), inline = 'false')
+                            embed.add_field(name = 'Type of Event', value = '{}'.format(finalToE), inline = 'false')
+                            embed.add_field(name = 'Proof', value = '{}'.format(finalProof), inline = 'false')
+                            
+                            await postChannel.send(embed=embed)
+                            await postChannel.send('<@&663190515323371540> <@&663192507936538664>')
+                            await channel.send(':white_check_mark: Event successfully posted, '+message.author.mention)
+
+                        else:
+                            await channel.send('Improper formatting, '+message.author.mention)
+                        
+                    else:
+                        await channel.send('Improper formatting, '+message.author.mention)
+                    
+                else:
+                    await channel.send('Improper formatting, '+message.author.mention)
+    
+            else:
+                await channel.send('Improper formatting, '+message.author.mention)
+            
+            
+        else:
+            pass
+    
 client.run(discordToken)
-
-
-
-
